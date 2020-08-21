bug fix: threshold datatype computation was being overwritten by attribute

src/finn/custom_op/fpgadataflow/streamingfclayer_batch.py
@@ -681,8 +681,8 @@ class StreamingFCLayer_Batch(HLSCustomOp):
                 else:
                     tdt = DataType.get_smallest_possible(tdt_max)
 
-                # get computed threshold datatype from attribute
-                tdt = DataType[self.get_nodeattr("accDataType")]
+                # # get computed threshold datatype from attribute
+                # tdt = DataType[self.get_nodeattr("accDataType")]
 
                 assert np.vectorize(tdt.allowed)(
                     threshold_tensor